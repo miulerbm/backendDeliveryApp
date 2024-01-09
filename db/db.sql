7b1776b6f64326ce664ecf79f74553752baf9ba1
miulerbm <miulerbm00@gmail.com>
2024-01-09 16:49:13 -0500

    Changing the image to null allowed

--- db/db.sql
+++ db/db.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     name VARCHAR(90) NOT NULL,
     lastname VARCHAR(90) NOT NULL,
     phone VARCHAR(90) NOT NULL UNIQUE,
-    image VARCHAR(255) NOT NULL,
+    image VARCHAR(255) NULL,
     password VARCHAR(90) NOT NULL,
     created_at TIMESTAMP(0) NOT NULL,
     updated_at TIMESTAMP(0) NOT NULL
